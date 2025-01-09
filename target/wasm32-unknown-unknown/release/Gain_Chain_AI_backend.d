7fa43d8bf534bc31818da6b352bfeb28afb1f1af
him-linux <bwires947@gmail.com>
2025-01-09 17:16:13 +0300

    Add initial configuration and service definitions for Gain Chain AI canisters

--- target/wasm32-unknown-unknown/release/Gain_Chain_AI_backend.d
+++ target/wasm32-unknown-unknown/release/Gain_Chain_AI_backend.d
@@ -1,1 +1,1 @@
-/home/him/coding_projects/Gain_Chain_AI/target/wasm32-unknown-unknown/release/Gain_Chain_AI_backend.wasm: /home/him/coding_projects/Gain_Chain_AI/src/Gain_Chain_AI_backend/src/lib.rs
+/home/him/coding_projects/junk/Gain_Chain_AI/target/wasm32-unknown-unknown/release/Gain_Chain_AI_backend.wasm: /home/him/coding_projects/junk/Gain_Chain_AI/src/Gain_Chain_AI_backend/src/lib.rs
